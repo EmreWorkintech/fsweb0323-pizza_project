1c7ef96f50cc22b59e5afcfdbb157ccdddc6454f
EmreSahiner <emre@journeyapp.com>
2023-07-10 01:04:05 -0700

    backend security measures implemented

--- api/server.js
+++ api/server.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const server = express();
 require('dotenv').config();
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 
 
 //2. global middleware'larım
-
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
 
 
 //3. Router'larım
